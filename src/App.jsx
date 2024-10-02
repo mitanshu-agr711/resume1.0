0825e0223e1eaba42dceaf63010e9efd1a1ac45d
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2024-10-02 16:30:03 +0530

    now navbar is added

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ResumeState from './context/resume.context.jsx';
+// import Home from './Pages/Home/Home';
+// import Navbar from './Components/Navbar/Navbar';
+import About from './pages/about.jsx';
+
+function App() {
+  return (
+    <ResumeState>
+      <div className="App">
+        <Helmet>
+          <title>Resume Builder - Create Professional Resumes Online</title>
+          <meta name="description" content="Build and customize professional resumes online with Resume Builder. Choose from a variety of templates and create your perfect resume easily." />
+          <meta name="keywords" content="resume builder, professional resumes, online resumes, resume templates" />
+          <meta name="author" content="Mitanshu Agrawal" />
+        </Helmet>
+        {/* <Navbar /> */}
+        <Routes>
+          {/* <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} /> */}
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </ResumeState>
+  );
+}
+
+export default App;
